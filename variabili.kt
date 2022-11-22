6262c052647deb496a0a85e8f3858139da6e0278
AvyonWar <110771481+AvyonWar@users.noreply.github.com>
2022-11-22 11:37:50 +0100

    Create variabili.kt
    ti ho creato direttamente qua il file da github copiandolo da kotlin pgroud.
    spero di averlo creato giusto

---
+++ variabili.kt
@@ -0,0 +1,12 @@
+fun main(){
+    var a1 = "gina"
+    var age = 22
+    var newGlasses = false
+    println("Hello $a1, you have $age years old. Have you a same glasses? $newGlasses")
+    println("hello ${a1.length}")
+    
+    val num1 = 5
+    val num2 = 6
+    val result = num1 + num2
+    println("Result is $result")
+}
